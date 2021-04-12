62ef663d5e0ef3c8091f53be187cdb07f2bd0bbf
ty要nx <ttyy373172968@163.com>
2021-04-12 19:47:33 +0800

    deploy

---
+++ precache-manifest.dec24f8817677d0d7713dae803de86ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40bddc71029f95fbbd1",
+    "url": "/vue-money-website/css/app.ff28f378.css"
+  },
+  {
+    "revision": "b09f8d07cf4c7ff2733e1cb9962998cc",
+    "url": "/vue-money-website/index.html"
+  },
+  {
+    "revision": "a40bddc71029f95fbbd1",
+    "url": "/vue-money-website/js/app.7a97e8ac.js"
+  },
+  {
+    "revision": "f25710b08fd68607861c",
+    "url": "/vue-money-website/js/chunk-vendors.60c2506a.js"
+  },
+  {
+    "revision": "6f0359f9b95875aa04f9b01af963707c",
+    "url": "/vue-money-website/manifest.json"
+  },
+  {
+    "revision": "89e1f00de740f40e4c399c455c09611c",
+    "url": "/vue-money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-money-website/robots.txt"
+  }
+]);
